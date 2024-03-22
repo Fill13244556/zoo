4ddf059710d11c31fd8d22c260a5b5a08de4ab84
Fill13244556 <114169957+Fill13244556@users.noreply.github.com>
2024-03-22 16:19:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+const slider = document.getElementById('slider');
+const reviews = document.querySelector('.review_list2').children;
+
+slider.addEventListener('input', function() {
+    const value = this.value;
+    for (let i = 0; i < reviews.length; i++) {
+        if (i < value) {
+            reviews[i].classList.add('.review_1');
+        } else {
+            reviews[i].classList.remove('show');
+        }
+    }
+});
+
+
 $(function() {
     $(".btn-submit").on("click", validate);
    
-  
+    // Validate email
     function validateEmail(email) {
       var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
       return re.test(String(email).toLowerCase());
     }
      
- 
+    // send form
     function sendForm() {
       $(".error").text("Form sending").fadeIn();
     }
    
+    // validate email and send form after success validation
     function validate() {
       var email = $(".email").val();
       var $error = $(".error");
@@ -26,4 +42,16 @@ $(function() {
       }
       return false;
     }
+  });
+
+
+
+
+  let button = document.querySelector('.circle_button');
+  let colors = 'orange';
+  let currentColorIndex = 0;
+ 
+  button.addEventListener('click', function() {
+      button.className = 'circle_button ' + colors[currentColorIndex];
+      currentColorIndex = (currentColorIndex + 1) % colors.length;
   });
